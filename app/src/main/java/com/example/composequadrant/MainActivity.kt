00084d64aad2e87405679a4e868c9c361a24b420
reda.ghoussoub@gmail.com <reda.ghoussoub@gmail.com>
2023-04-11 16:37:40 -0400

    Initial commit

---
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -0,0 +1,109 @@
+package com.example.composequadrant
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeQuadrantTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ComposeQuadrantApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposeQuadrantApp() {
+    Column(Modifier.fillMaxSize()) {
+        Row(Modifier.weight(1f)) {
+            QuadrantInfoCard(
+                title = stringResource(id = R.string.text_composable),
+                description = stringResource(id = R.string.text_composable_description),
+                backgroundColor = Color.Green,
+                modifier = Modifier.weight(1f)
+            )
+            QuadrantInfoCard(
+                title = stringResource(id = R.string.image_composable),
+                description = stringResource(id = R.string.image_composable_description),
+                backgroundColor = Color.Yellow,
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            QuadrantInfoCard(
+                title = stringResource(id = R.string.row_composable),
+                description = stringResource(id = R.string.row_composable_description),
+                backgroundColor = Color.Cyan,
+                modifier = Modifier.weight(1f)
+            )
+            QuadrantInfoCard(
+                title = stringResource(id = R.string.column_composable),
+                description = stringResource(id = R.string.column_composable_description),
+                backgroundColor = Color.LightGray,
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun QuadrantInfoCard(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify,
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun ComposeQuadrantPreview() {
+    ComposeQuadrantTheme {
+        ComposeQuadrantApp()
+    }
+}
